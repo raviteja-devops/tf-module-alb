f59c4c7c336a23e0764e433da427d746ba092e55
raviteja <ravitejatfcdemo@gmail.com>
2023-02-28 12:43:41 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -33,3 +33,21 @@ resource "aws_lb" "main" {
     { Name = "${var.env}-${var.subnets_name}-alb" }
   )
 }
+
+
+resource "aws_lb_listener" "backend" {
+  count             = var.internal ? 1 : 0
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "NO_RULE"
+      status_code  = "503"
+    }
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "dns_name" {
   value = aws_lb.main.dns_name
 }
+
+output "listener" {
+  value = try(aws_lb_listener.backend.*.arn[0], null)
+}
+
+output "alb_arn" {
+  value = aws_lb.main.arn
+}
